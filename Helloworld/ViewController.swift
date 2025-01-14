d32697949a9b094671457954b837e48f8a6632a2
moonaneul <melody0404@naver.com>
2025-01-14 15:39:22 +0900

    Initial Commit

---
+++ Helloworld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Helloworld
+//
+//  Created by Haneul Mun on 1/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloworldTests/HelloworldTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloworldTests.swift
+//  HelloworldTests
+//
+//  Created by Haneul Mun on 1/14/25.
+//
+
+import Testing
+@testable import Helloworld
+
+struct HelloworldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
